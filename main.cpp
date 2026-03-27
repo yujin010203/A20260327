3cc3d1f32334d6c4488e9111b623d80fc2bf543d
yujin010203 <yyuyu02@naver.com>
2026-03-27 17:24:54 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,11 +28,54 @@ public:
 
 Singleton* Singleton::Instance = nullptr;
 
+
+void SortSelection(int* Array)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (Array[i] < Array[j])
+			{
+				int temp = Array[i];
+				Array[i] = Array[j];
+				Array[j] = temp;
+			}
+		}
+	}
+}
+
+void SortBubble(int Array[], int Number)
+{
+	for (int i = 0; i < Number - 1; i++)
+	{
+		for (int j = 0; j < Number - 1 - i; j++)
+		{
+			if (Array[j] > Array[j + 1])
+			{
+				int temp = Array[j];
+				Array[j] = Array[j + 1];
+				Array[j + 1] = temp;
+			}
+		}
+	}
+}
+
 int main()
 {
-	GEngine->GetInstance();
-	GEngine->GetWorld()->Load("level01.umap");
-	GEngine->Run();
+	//GEngine->GetInstance();
+	//GEngine->GetWorld()->Load("level01.umap");
+	//GEngine->Run();
+
+	int Data[10] = { 9, 1, 3, 5, 4, 6, 7, 8, 2, 10};
+
+	//search and sort
+	SortBubble(Data, 10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << Data[i] << " ";
+	}
 
 	return 0;
 }
